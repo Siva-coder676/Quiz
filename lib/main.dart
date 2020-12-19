d3a82edc7ac8f4c94c5e01f22a46516e4b4d0019
Siva-coder676 <76256930+Siva-coder676@users.noreply.github.com>
2020-12-19 23:39:00 +0530

    Add files via upload

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'file:///E:/projectsworkspace/quiz/lib/UI/Pages/Pages/homePage.dart';
+
+void main() {
+  runApp(MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: "Quiz",
+      theme: ThemeData(
+          primarySwatch: Colors.pink,
+          accentColor: Colors.indigo,
+          fontFamily: "Montserrat",
+          buttonColor: Colors.pink,
+          buttonTheme: ButtonThemeData(
+            buttonColor: Colors.pink,
+            shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(20.0)),
+            textTheme: ButtonTextTheme.primary,
+          )),
+      home: HomePage()));
+}
