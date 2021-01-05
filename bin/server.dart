6531cbc869998f6476e013e33dcdf42adc6318df
unknown <sivaprajee@gmail.com>
2021-01-05 20:13:29 +0530

    server file added

---
+++ bin/server.dart
@@ -0,0 +1,31 @@
+import 'dart:io';
+import 'package:args/args.dart';
+import 'package:shelf/shelf.dart' as shelf;
+import 'package:shelf/shelf_io.dart' as io;
+import 'package:shelf_static/shelf_static.dart';
+var portEnv = Platform.environment['PORT'];
+var _hostname = portEnv == null ? 'localhost' : '0.0.0.0';
+
+void main(List<String> args) async {
+  var parser = ArgParser()..addOption('port', abbr: 'p');
+  var result = parser.parse(args);
+
+  var portStr = result['port'] ?? portEnv ?? '8080';
+  var port = int.tryParse(portStr);
+
+  if (port == null) {
+    stdout.writeln('Could not parse port value "$portStr" into a number.');
+    exitCode = 64;
+    return;
+  }
+
+  var handler = const shelf.Pipeline()
+      .addMiddleware(shelf.logRequests())
+      .addHandler(_echoRequest);
+
+  var server = await io.serve(handler, _hostname, port);
+  print('Serving at http://${server.address.host}:${server.port}');
+}
+
+shelf.Response _echoRequest(shelf.Request request) =>
+    shelf.Response.ok('Request for "${request.url}"');
